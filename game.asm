f6a4cc92080bb511127a4d1ea1ab3b1a626bcdec
Bautista <bautistajosebona@gmail.com>
2024-09-12 20:20:26 -0300

    arreglado el movimiento de la nave, disparo y alienigenas

--- game.asm
+++ game.asm
@@ -1,82 +1,506 @@
 .ORIG x3000
-    LD R0, rect_pos       
-    LD R1, white          
-    LD R2, negro          
-    LD R3, row_width      
-    LD R4, rect_width     
-    LD R5, rect_height    
-    LEA R6, current_pos   
-    STR R0, R6, #0        
-    
-MAIN_LOOP
-    LDR R0, R6, #0        
-    JSR DRAW_RECTANGLE    
-    JSR READ_INPUT        
-    JSR MOVE_RECTANGLE    
-    BR MAIN_LOOP         
 
-DRAW_RECTANGLE
-    ST R7, return_address
-DRAW_RECTANGLE_RETUR
-    LD R7, rect_width     
-DRAW_RECTANGLE_ROW
-    STR R1, R0, #0        ; Dibujar el píxel
-    ADD R0, R0, #1        ; Mover a la derecha
-    ADD R7, R7, #-1       ; Decrementar el contador de píxeles en la fila
-    BRp DRAW_RECTANGLE_ROW ; Continuar dibujando en la misma fila
+MAIN	JSR	LIMPIAR_PANTALLA
+	JSR	SETUP_DATA
+	JSR	REDIBUJAR_PANTALLA
+	JSR	GAME_LOOP
+	PUTS
+	HALT
 
-    ; Mover a la siguiente fila
-    ADD R0, R0, #-10      ; Volver al primer píxel de la fila
-    ADD R0, R0, R3        ; Mover a la siguiente fila
-    ADD R5, R5, #-1       ; Decrementar el contador de filas
-    BRp DRAW_RECTANGLE_RETUR    ; Si quedan filas por dibujar, repetir
-    LD R7, return_address
-    RET                   ; Regresar al bucle principal
+SETUP_DATA	ST	R0, SD_R0
+		ST	R0, SD_R1
+		ST	R0, SD_R2
+		ST	R0, SD_R3
+		ST	R0, SD_R4
+		ST	R7, SD_R7
+		LEA	R0, ALIEN0			
+		LD	R1, ALIEN_START
+		LD	R2, SD_AZUL
+		LD	R3, ALIEN_OFFSET
+		AND	R4, R4 ,#0
+		ADD	R4, R4 ,#4			
+		
+INIT_ALIENS	STR	R2, R0 ,#0			
+		ADD	R0, R0 ,#1			
+		STR	R1, R0 ,#0			
+		ADD	R1, R1, R3			
+		ADD	R0, R0 ,#1			
+		ADD	R4, R4 ,#-1			
+		BRp	INIT_ALIENS
+		LEA	R0, NAVE
+		LD	R1, NAVE_START
+		LD	R2, SD_ROJO
+		STR	R2, R0 ,#0			
+		ADD	R0, R0 ,#1			
+		STR	R1, R0 ,#0			
+		LEA	R0, LASER
+		AND	R1, R1 ,#0
+		STR	R1, R0 ,#0			
+		ADD	R0, R0 ,#1
+		ADD	R1, R1 ,#1
+		STR	R1, R0 ,#0			
+		LD	R0, SD_R0
+		LD	R0, SD_R1
+		LD	R0, SD_R2
+		LD	R0, SD_R3
+		LD	R0, SD_R4
+		LD	R7, SD_R7
+		RET
+SD_R0	.BLKW 1
+SD_R1	.BLKW 1
+SD_R2	.BLKW 1
+SD_R3	.BLKW 1
+SD_R4	.BLKW 1
+SD_R7	.BLKW 1
+ALIEN_START	.FILL xC18A
+NAVE_START	.FILL xF3B3
+ALIEN_OFFSET	.FILL #30
+SD_AZUL		.FILL x001F
+SD_ROJO		.FILL x7C00
 
-READ_INPUT
-    LD R2, KBD_BUF        
-    RET                   
+DIBUJAR_NAVE	ST	R0, DSH_R0
+		ST	R3, DSH_R3
+		ST	R4, DSH_R4
+		ST	R7, DSH_R7
+		AND	R4, R4 ,#0		
+		ADD	R4, R2 ,#0		
+		LD	R2, NAVE_ANCHO
+		LD	R3, NAVE_LARGO
+		JSR	DIBUJAR_CUADRADO		
+		LD	R0, DSH_R0
+		LD	R3, DSH_R3
+		LD	R4, DSH_R4
+		LD	R7, DSH_R7
+		RET
+DSH_R0		.FILL 1
+DSH_R3		.FILL 1
+DSH_R4		.FILL 1
+DSH_R7		.FILL 1
+NAVE_ANCHO	.FILL #24
+NAVE_LARGO	.FILL #12
 
-MOVE_RECTANGLE
-    ST R7, return_address
-    LD R2, KBD_BUF        
-    LDR R1, R6, #0        
-    ; Borrar el rectángulilo en la posición actual antes de moverlo
-    LD R2, negro
-    JSR DRAW_RECTANGLE
-    
-    ; Mover el rectángulo en función de la tecla presionada
-    LD R7, left_key       ; Cargar la tecla 'A'
-    BRz MOVE_LEFT
-    LD R7, right_key      ; Cargar la tecla 'D'
-    BRz MOVE_RIGHT
-    LD R7, return_address
-    RET                   ; Si no es una tecla de movimiento, regresar
+DIBUJAR_ALIENS	ST	R0, DA_R0
+		ST	R1, DA_R1
+		ST	R2, DA_R2
+		ST	R3, DA_R3
+		ST	R4, DA_R4
+		ST	R5, DA_R5
+		ST	R7, DA_R7
+		AND	R0, R0 ,#0		
+		ADD	R0, R0 ,#4	
+		LEA	R5, ALIEN0		
+DIBUJAR_ALIEN	LDR	R4, R5 ,#0		
+		ADD 	R5, R5 ,#1		
+		LDR 	R1, R5 ,#0
+		LD	R2, ALIEN_DIM	
+		LD	R3, ALIEN_DIM	
+		JSR	DIBUJAR_CUADRADO		
+		ADD	R5, R5 ,#1		
+		ADD	R0, R0 ,#-1		
+		BRp	DIBUJAR_ALIEN
+		LD	R0, DA_R0
+		LD	R1, DA_R1
+		LD	R2, DA_R2
+		LD	R3, DA_R3
+		LD	R4, DA_R4
+		LD	R5, DA_R5
+		LD	R7, DA_R7
+		RET
 
-MOVE_LEFT
-    ADD R1, R1, #-1       
-    BR MOVE_END
-    
-MOVE_RIGHT
-    ADD R1, R1, #1     
-    BR MOVE_END
+DA_R0		.BLKW 1
+DA_R1		.BLKW 1
+DA_R2		.BLKW 1
+DA_R3		.BLKW 1
+DA_R4		.BLKW 1
+DA_R5		.BLKW 1
+DA_R7		.BLKW 1
+ALIEN_DIM	.FILL #14
 
-MOVE_END
-    STR R1, R6, #0        ; Guardar la nueva posición del rectángulo
-    LD R1, white          ; Cargar el color blanco para dibujar el rectángulo
-    JSR DRAW_RECTANGLE    ; Dibujar el rectángulo en la nueva posición
-    RET                   ; Regresar al bucle principal
+DIBUJAR_LASER	ST	R3, DL_R3
+		ST	R4, DL_R4
+		ST	R7, DL_R7
+		AND	R4, R4 ,#0
+		ADD	R4, R2 ,#0
+		LD	R2, ANCHO_LASER
+		LD	R3, LARGO_LASER
+		JSR	DIBUJAR_CUADRADO		
+		LD	R3, DL_R3
+		LD	R4, DL_R4
+		LD	R7, DL_R7
+		RET
 
-; Constantes y datos
-return_address .FILL xFFFF 
-white .FILL xFFFF         
-negro .FILL x0000         
-rect_pos .FILL xF43C      
-row_width .FILL #128      
-rect_width .FILL #10      
-rect_height .FILL #4      
-left_key .FILL x61        ; Tecla 'A' para mover a la izquierda
-right_key .FILL x64       ; Tecla 'D' para mover a la derecha
-KBD_BUF .FILL xFE02           ; Buffer para almacenar la tecla presionada
-current_pos .BLKW 1       ; Posición actual del rectángulo
+ANCHO_LASER	.FILL #2
+LARGO_LASER	.FILL #12
+DL_R3		.BLKW 1
+DL_R4		.BLKW 1
+DL_R7		.BLKW 1
+
+REDIBUJAR_PANTALLA	ST	R0, SS_R0
+		ST	R1, SS_R1
+		ST	R2, SS_R2
+		ST	R3, SS_R3
+		ST	R4, SS_R4
+		ST	R5, SS_R5
+		ST	R7, SS_R7
+		JSR	LIMPIAR_PANTALLA	
+		JSR	DIBUJAR_ALIENS
+		LEA	R5, NAVE
+		LDR	R2, R5 ,#0		
+		ADD	R5, R5 ,#1		
+		LDR 	R1, R5 ,#0		
+		JSR	DIBUJAR_NAVE		
+		LD	R0, SS_R0
+		LD	R1, SS_R1
+		LD	R2, SS_R2
+		LD	R3, SS_R3
+		LD	R4, SS_R4
+		LD	R5, SS_R5
+		LD	R7, SS_R7
+		RET
+		
+SS_R0	.BLKW 1
+SS_R1	.BLKW 1
+SS_R2	.BLKW 1
+SS_R3	.BLKW 1
+SS_R4	.BLKW 1
+SS_R5	.BLKW 1
+SS_R7	.BLKW 1
+
+GAME_LOOP	ST	R7, GL_R7
+GAME		JSR	TIMED_INPUT 
+   
+SKIP_WHITE	LD	R1, N97        
+		ADD	R1, R0, R1    
+		BRnp	SKIP_LEFT     
+		AND	R0, R0 ,#0
+		ADD	R1, R1 ,#-4
+		JSR	MOVE_NAVE
+		
+SKIP_LEFT	LD	R1, N100       
+		ADD	R1, R0, R1    
+		BRnp	SKIP_RIGHT    
+		AND	R0, R0 ,#0
+		ADD	R1, R1 ,#4	
+		JSR	MOVE_NAVE
+				
+SKIP_RIGHT	LD	R1, N32        
+		ADD	R1, R0, R1    
+		BRnp	SKIP_QUIT
+		JSR	SHOOT		
+			
+SKIP_QUIT	JSR 	ANIMAR_LASER
+		BRnzp	GAME        
+		  
+QUIT		LD	R7, GL_R7     
+			RET
+
+			
+N97	.FILL #-97	; a
+N100	.FILL #-100	; d
+N32	.FILL #-32	; espacio
+GL_R7	.BLKW 1
+ROJO	.FILL x7C00
+GREEN	.FILL x03E0
+AZUL	.FILL x001F
+NEGRO	.FILL x0000
+
+; Objetos del juego
+ALIEN0	.BLKW 2	
+ALIEN1	.BLKW 2
+ALIEN2	.BLKW 2
+ALIEN3	.BLKW 2
+NAVE	.BLKW 2 
+LASER	.BLKW 2 
+
+;; Mover la nave
+MOVE_NAVE	ST	R0, MS_R0
+		ST	R2, MS_R2
+		ST	R3, MS_R3
+		ST	R4, MS_R4
+		ST	R5, MS_R5
+		ST	R7, MS_R7
+		LEA	R0, NAVE
+		ADD	R0, R0 ,#1		
+		LDR	R4, R0 ,#0		
+		ADD	R5, R1, R4		
+		LD	R3, NAVE_MIN
+		ADD	R3, R5, R3		
+		BRn	NO_MOVE
+		LD	R3, NAVE_MAX
+		ADD	R3, R5, R3		
+		BRp	NO_MOVE
+		STR	R5, R0 ,#0		
+		AND	R1, R1 ,#0		
+		ADD	R1, R4 ,#0		
+		LD	R2, MS_NEGRO	
+		JSR	DIBUJAR_NAVE
+		AND	R1, R1 ,#0		
+		ADD	R1, R5 ,#0		
+		ADD	R0, R0 ,#-1		
+		LDR	R2, R0 ,#0	
+		JSR	DIBUJAR_NAVE
+		
+NO_MOVE		LD	R0, MS_R0
+		LD	R2, MS_R2
+		LD	R3, MS_R3
+		LD	R4, MS_R4
+		LD	R5, MS_R5
+		LD	R7, MS_R7
+		RET
+		
+MS_R0	.BLKW 1
+MS_R2	.BLKW 1
+MS_R3	.BLKW 1
+MS_R4	.BLKW 1
+MS_R5	.BLKW 1
+MS_R7	.BLKW 1
+NAVE_MAX	.FILL x0C19 
+NAVE_MIN	.FILL x0C7D 
+MS_NEGRO	.FILL x0000
+
+SHOOT	ST	R0, S_R0
+	ST	R1, S_R1
+	ST	R2, S_R2
+	ST	R3, S_R3
+	ST	R4, S_R4
+	ST	R7, S_R7
+	LEA	R0, LASER
+	LDR	R1, R0 ,#0 
+	BRp	NOSHOOT
+	AND	R1, R1 ,#0
+	ADD	R1, R1 ,#1
+	STR	R1, R0 ,#0	
+	LEA	R2, NAVE
+	LDR	R2, R2 ,#1	
+	ADD	R2, R2 ,#11	
+	LD	R3, NLASER_Y
+	ADD	R2, R2, R3	
+	AND	R1, R1 ,#0
+	ADD	R1, R2 ,#0	
+	STR	R1, R0 ,#1	
+	LD	R2, S_GREEN
+	JSR	DIBUJAR_LASER
+	
+NOSHOOT	LD	R0, S_R0
+	LD	R1, S_R1
+	LD	R2, S_R2
+	LD	R3, S_R3
+	LD	R4, S_R4
+	LD	R7, S_R7
+	RET
+
+S_R0	.BLKW 1
+S_R1	.BLKW 1
+S_R2	.BLKW 1
+S_R3	.BLKW 1
+S_R4	.BLKW 1
+S_R7	.BLKW 1
+NLASER_Y	.FILL xFA00 
+CSH_R0		.BLKW 1
+CSH_R1		.BLKW 1
+CSH_R2		.BLKW 1
+CSH_R3		.BLKW 1
+CSH_R4		.BLKW 1
+CSH_R6		.BLKW 1
+CSH_R7		.BLKW 1
+NALIEN		.FILL #-17	
+NNAVE0_0	.FILL #-9	
+NNAVE0_1	.FILL #-24	
+NNAVE1_0	.FILL #-39	
+NNAVE1_1	.FILL #-54	
+NNAVE2_0	.FILL #-69	
+NNAVE2_1	.FILL #-84	
+NNAVE3_0	.FILL #-99	
+NNAVE3_1	.FILL #-114
+CSH_ROJO	.FILL x7C00
+
+;; Animar Laser
+ANIMAR_LASER	ST	R0, AL_R0
+		ST	R1, AL_R1
+		ST	R2, AL_R2
+		ST	R3, AL_R3
+		ST	R4, AL_R4
+		ST	R5, AL_R5
+		ST	R7, AL_R7
+		LEA	R0, LASER
+		LDR	R1, R0 ,#0
+		BRnz	END_ANIMATE			
+		LDR	R1, R0 ,#1			
+		LD	R2, NLASER_OFFSET
+		ADD	R2, R1, R2			
+		JSR	CONVERTIR_TO_XY		
+		ADD	R4, R4 ,#0
+		BRnz	CLEAR_LASER			
+		ADD	R5, R5 ,#0			
+		BRnz	ANIMATE
+
+CLEAR_LASER	LD	R2, AL_NEGRO
+		JSR	DIBUJAR_LASER
+		AND	R1, R1 ,#0			
+		STR	R1, R0 ,#0			
+		BRnzp	END_ANIMATE
+		
+ANIMATE		AND	R6, R6 ,#0
+		ADD	R6, R2 ,#0			
+		LD	R2, AL_NEGRO
+		JSR	DIBUJAR_LASER			
+		AND	R1, R1 ,#0			
+		ADD	R1, R6 ,#0			
+		STR	R1, R0 ,#1			
+		LD	R2, AL_GREEN
+		JSR	DIBUJAR_LASER		
+
+END_ANIMATE	LD	R0, AL_R0
+		LD	R1, AL_R1
+		LD	R2, AL_R2
+		LD	R3, AL_R3
+		LD	R4, AL_R4
+		LD	R5, AL_R5
+		LD	R7, AL_R7
+		RET
+
+AL_R0	.BLKW 1
+AL_R1	.BLKW 1
+AL_R2	.BLKW 1
+AL_R3	.BLKW 1
+AL_R4	.BLKW 1
+AL_R5	.BLKW 1
+AL_R7	.BLKW 1
+NLASER_OFFSET	.FILL #-768 
+AL_NEGRO		.FILL x0000
+
+;; Volver pantalla a negro
+LIMPIAR_PANTALLA	ST	R7, CS_R7
+		LD	R0, PIXELS
+		LD	R1, SCREEN_START
+		LD	R2, CS_NEGRO
+		
+CLEAR_LOOP	STR	R2, R1 ,#0
+		ADD	R1, R1 ,#1
+		ADD	R0, R0 ,#-1
+		BRp	CLEAR_LOOP
+		LD	R7, CS_R7
+		RET
+
+CS_NEGRO	.FILL x0000
+SCREEN_START	.FILL xC000
+PIXELS		.FILL #15872
+CS_R7		.BLKW 1
+
+
+;; Convertir los pixeles a coordenadas x e y
+CONVERTIR_TO_XY	ST	R0, CTXY_R0
+		ST	R1, CTXY_R1
+		ST	R2, CTXY_R2
+		ST	R5, CTXY_R5
+		ST	R7, CTXY_R7
+		LD	R3, PX_OFFSET
+		ADD	R2, R2, R3
+		AND	R3, R3 ,#0
+		AND	R4, R4 ,#0
+		LD	R0, N128
+		
+CONVERTIR_LOOP	ADD	R5, R2, R0
+		BRn	END_CONVERTIR
+		ADD	R4, R4 ,#1		
+		ADD	R2, R2, R0
+		BRnzp	CONVERTIR_LOOP
+		
+END_CONVERTIR	ADD	R3, R2 ,#0		
+		LD	R0, CTXY_R0
+		LD	R1, CTXY_R1
+		LD	R2, CTXY_R2
+		LD	R5, CTXY_R5
+		LD	R7, CTXY_R7
+		RET
+		
+CTXY_R0		.BLKW 1
+CTXY_R1		.BLKW 1
+CTXY_R2		.BLKW 1
+CTXY_R5		.BLKW 1
+CTXY_R7		.BLKW 1
+PX_OFFSET	.FILL #16384
+N128		.FILL #-128 
+
+;; TIMED_INPUT
+TIMED_INPUT	ST	R1, TI_R0
+		ST	R2, TI_R2
+		ST	R3, TI_R3
+		ST	R7, TI_R7
+		AND	R0, R0 ,#0
+		LD	R2, TICKS
+		STI	R2, TMI
+POLL		LDI	R3, TMR
+		BRn	EXIT_INPUT
+		LDI	R1, KBSR
+		BRzp	POLL
+		LDI	R0, KBDR
+		LD	R1, TI_R0
+		LD	R2, TI_R2
+		LD	R3, TI_R3
+		LD	R7, TI_R7
+EXIT_INPUT	RET
+
+TI_R0	.BLKW 1
+TI_R2	.BLKW 1
+TI_R3	.BLKW 1
+TI_R7	.BLKW 1
+KBSR	.FILL xFE00 
+KBDR	.FILL xFE02 
+TMR	.FILL xFE08	
+TMI	.FILL xFE0A 
+TICKS	.FILL x00C8 
+
+;; Dibujar alien
+DIBUJAR_CUADRADO	ST	R0, DS_R0
+		ST	R5, DS_R5
+		ST	R6, DS_R6
+		ST	R7, DS_R7
+		LD	R5, ROW_OFFSET
+		NOT	R2, R2
+		ADD	R2, R2 ,#1
+		ST	R2, NANCHO	
+		NOT	R3, R3
+		ADD	R3, R3 ,#1
+		ST	R3, NLARGO	
+		AND 	R0, R0 ,#0
+		
+COL	ST	R0, DS_I
+	LD	R3, NLARGO
+	ADD	R0, R0, R3	
+	BRzp	END_COL
+	AND 	R0, R0 ,#0
+	
+ROW	ST	R0, DS_J
+	LD	R3, NANCHO
+	ADD	R0, R0, R3	
+	BRzp	END_ROW
+	LD	R0, DS_J
+	AND	R6, R6 ,#0	
+	ADD	R6, R1, R0	
+	STR	R4, R6 ,#0	
+	ADD	R0, R0 ,#1	
+	BR	ROW
+	
+END_ROW	ADD	R1, R1, R5	
+	LD	R0, DS_I
+	ADD	R0, R0 ,#1	
+	BR	COL
+	
+END_COL	LD	R0, DS_R0
+	LD	R5, DS_R5
+	LD	R6, DS_R6
+	LD	R7, DS_R7
+	RET
+
+DS_R0		.FILL 1
+DS_R5		.FILL 1
+DS_R6		.FILL 1
+DS_R7		.FILL 1
+ROW_OFFSET	.FILL x0080
+DS_I		.BLKW 1
+DS_J		.BLKW 1
+NANCHO		.BLKW 1
+NLARGO		.BLKW 1
+
 .END
